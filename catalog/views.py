26232862becf98ed95c6514438f9c566e63f4803
Stephen Wentling <swentling87@gmail.com>
2018-02-27 01:21:10 -0800

    ongoing tutorial progress

--- catalog/views.py
+++ catalog/views.py
@@ -13,10 +13,10 @@ def index(request):
     # Available books (status = 'a')
     num_instances_available=BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()  # The 'all()' is implied by default.
-    
+    num_genres=Genre.objects.count()
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,'num_genres':num_genres },
     )
