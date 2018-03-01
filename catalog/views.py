74a8edd4af780be70049bee4da9bb6680854818a
Stephen Wentling <swentling87@gmail.com>
2018-03-01 00:37:02 -0800

    section 6 added and changed to python3

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """
@@ -20,3 +20,16 @@ def index(request):
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,'num_genres':num_genres },
     )
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    
+class AuthorDetailView(generic.DetailView):
+    model = Author
