267a61b947e00035dd00879d236f671071c7e3b3
Stephen Wentling <swentling87@gmail.com>
2018-03-22 23:44:31 -0700

    added book methods

--- catalog/views.py
+++ catalog/views.py
@@ -112,3 +112,15 @@ class AuthorUpdate(UpdateView):
 class AuthorDelete(DeleteView):
     model = Author
     success_url = reverse_lazy('authors')
+    
+class BookCreate(CreateView):
+    model = Book
+    fields = '__all__'
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = ['title','author','summary','isbn','genre','language']
+
+class BookDelete(DeleteView):
+    model = Book
+    success_url = reverse_lazy('authors')
